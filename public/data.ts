41838efd56b64ac2012a79a21e7e77e616c1bb64
Nicholas Cho <nicholascho@Nicholass-MacBook-Pro.local>
2025-08-12 00:42:28 -0700

    add more concerts

--- public/data.ts
+++ public/data.ts
@@ -267,6 +267,265 @@
       }
     ]
   },
+    {
+    id: 6,
+    artist_name: "RINI",
+    artist_photo: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754976621/cropped-R_B_5A_eo6dv0.jpg",
+    tour_name: "Somewhere in Time Tour",
+    date: "June 7th, 2025",
+    location: "San Francisco, CA",
+    venue: "The Regency Ballroom",
+    album_id: "14KJbhk4JG4s3Ez0GSxT7Z",
+    album_ranking: [
+      { track_no: 1, rank: 1 },
+      { track_no: 2, rank: 7 },
+      { track_no: 3, rank: 6 },
+      { track_no: 4, rank: 5 },
+      { track_no: 5, rank: 4 },
+      { track_no: 6, rank: 3 },
+      { track_no: 7, rank: 2 },
+    ],
+    concert_notes: [
+      "Second time seeing Rini! The first concert I went to was a very tiny venue, so it was good to see him fill out a bigger venue like Regency. Very last minute decision to go, but I'm glad I was able to convince Clarissa to go with me",
+      "The openers were good, but their sets and then the wait time in between was soooooo long :( I think the concert ended up being about 4 hours in total, I could tell it was a long time because my back hurt a lot by the end",
+      "Regardless, really good setlist, he hit all of the important songs lol. Also he did a lot of his collab songs too? I really didn't expect to hear Stay but it was great to hear live",
+      "I think compared to the first concert, I might have preferred this one just because he seemed to be in better condition this time. Vocals sounded great and he kept the energy up throughout the whole set",
+      "I had been wanting to see Omar since he first came to Berkeley to perform in 2018; back then I didn't know who he was so I didn't go! Ever since then I had been really regretful about that, so it was a bit of a cathartic moment to finally get to see him live"
+    ],
+    poster_image: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754976520/Screenshot_2025-08-11_at_10.24.18_PM_s8nm69.png",
+    videos: [
+      { 
+        title: "Scars",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976537/IMG_8722_yscmcg.mov"
+      },
+      { 
+        title: "My Luv",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976556/IMG_8723_nbe8tx.mov"
+      },
+      { 
+        title: "Out of the Blue",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976569/IMG_8724_bdtdcl.mov"
+      },
+      { 
+        title: "Red Lights",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976592/IMG_8725_cecds3.mov"
+      },
+      { 
+        title:"Stay",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754977131/IMG_8726_2_ky1fq7.mov"
+      },
+      { 
+        title: "Camped",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976893/IMG_8727_fkdiao.mov"
+      },
+      { 
+        title: "Emerald",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976582/IMG_8728_zzu5gx.mov"
+      },
+      { 
+        title:"Aphrodite",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976614/IMG_8736_kpmxbg.mov"
+      },
+      { 
+        title: "Far Away",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754976567/IMG_8740_a4fpqg.mov"
+      },
+    ]
+  },
+  {
+    id: 7,
+    artist_name: "Emotional Oranges",
+    artist_photo: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754978127/cropped-tumblr_0aacf4f93b39c462711fea66e220564c_a0ae501b_540_efztce.jpg",
+    tour_name: "The Pulp Fiction Tour 2023",
+    date: "March 29th, 2023",
+    location: "San Francisco, CA",
+    venue: "Warfield Theatre",
+    album_id: "6q8BNcH6wkWwWC0fGoJwkS",
+    album_ranking: [
+      { track_no: 1, rank: 7 },
+      { track_no: 2, rank: 4 },
+      { track_no: 3, rank: 6 },
+      { track_no: 4, rank: 8 },
+      { track_no: 5, rank: 3 },
+      { track_no: 6, rank: 1 },
+      { track_no: 7, rank: 2 },
+      { track_no: 8, rank: 5 },
+    ],
+    concert_notes: [
+      "Second time seeing Emotional Oranges! I remember after a while of indoctrinating Matt into liking Emotional Oranges he finally agreed to come to this concert with me and Denny hehe",
+      "They sounded so good, I really think V is such a good singer :o it sounds like there is autotune on her voice looking back at these videos. I remember that she really likes to do runs in their live performances so it's a completely new experience hearing songs live",
+      "Concert so good I bought a hoodie 😩 I still wear it super often too. I'm sooo glad that they did Not Worth It because I remember being really sad when they didn't do it at their previous concert. Such an underrated song"
+    ],
+    poster_image: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754978127/img_0648_nx4ukt.webp",
+    videos: [
+      { 
+        title: "She Got A Man",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754978643/IMG_0132_huxdrr.mov"
+      },
+      { 
+        title: "Not Worth It",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754978670/IMG_0133_2_wmhapc.mov"
+      },
+      { 
+        title: "Sundays",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754978650/IMG_0134_k3nldn.mov"
+      },
+      { 
+        title: "West Coast Love",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754978631/IMG_0135_v2arcn.mov"
+      },
+    ]
+  },
+    {
+    id: 8,
+    artist_name: "wave to earth",
+    artist_photo: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754979671/cropped-Wave_to_Earth_0.1_Flaws_and_All._promo_photo__281_29_mnmzss.webp",
+    tour_name: "North America Tour",
+    date: "August 18th, 2023",
+    location: "Berkeley, CA",
+    venue: "The UC Theatre",
+    album_id: "5T0Gt5JYXh6gEttuB8ujML",
+    album_ranking: [
+      { track_no: 1, rank: 3 },
+      { track_no: 2, rank: 7 },
+      { track_no: 3, rank: 2 },
+      { track_no: 4, rank: 9 },
+      { track_no: 5, rank: 13 },
+      { track_no: 6, rank: 11 },
+      { track_no: 7, rank: 5 },
+      { track_no: 8, rank: 4 },
+      { track_no: 9, rank: 6 },
+      { track_no: 10, rank: 12 },
+      { track_no: 11, rank: 10 },
+      { track_no: 12, rank: 14 },
+      { track_no: 13, rank: 1 },
+      { track_no: 14, rank: 8 },
+    ],
+    concert_notes: [
+      "I can be proud to say that I went to wave to earth's first US performance ever! This date was the kickoff to their first American tour and I can tell people were hyped because there were soooo many people (mostly college kids)",
+      "I can tell that they were pretty nervous, even Daniel who did most of the talking was pretty curt in all of his talks in between songs. It was kinda cute but also I can tell that there was still a bit of language barrier. But props to them for carrying the whole show without a translator",
+      "They sounded great of course, I wish they didn't rely so much on backtrack though. I know that they're only 3 people but I feel like I couldn't really hear the instruments that well. The breakdown of nouvelle vague was awesome though",
+      "So glad I brought Jordan to this concert and got him into wave to earth! Always so grateful whenever my friends are down to go to random concerts with me so I love you for that Jordan <3 and now he's a bigger fan than me lol"
+    ],
+    poster_image: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754979672/Wave_to_Earth_North_America_Tour_main_poster_fwtrcp.webp",
+    videos: [
+      { 
+        title: "bad",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754980500/IMG_1727_azyond.mov"
+      },
+      { 
+        title: "peach eyes",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754980510/IMG_1728_jbruhd.mov"
+      },
+            { 
+        title: "seasons",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754980504/IMG_1733_yd4tax.mov"
+      }
+    ]
+  },
+    {
+    id: 9,
+    artist_name: "IU",
+    artist_photo: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754981538/cropped-IU_The_Winning_album_cover__28Special_ver._29__282_29_r767w1.webp",
+    tour_name: "HEREH World Tour",
+    date: "July 30th, 2024",
+    location: "Oakland, CA",
+    venue: "Oakland Arena",
+    album_id: "08CvAj58nVMpq1Nw7T6maj",
+    album_ranking: [
+      { track_no: 1, rank: 2 },
+      { track_no: 2, rank: 1 },
+      { track_no: 3, rank: 4 },
+      { track_no: 4, rank: 3 },
+      { track_no: 5, rank: 5 },
+    ],
+    concert_notes: [
+      "I think this experience was really just a childhood dream come true... Honestly when she first came out on stage I couldn't believe that I was seeing her in person after having grown up with her music all throughout my life :')",
+      "We were in bleacher seats, but very close to the front so we were actually pretty close to the stage! I was surprised at how well I could see her even without looking at the screens",
+      "I think Matt, Francesca and I were just kinda freaking out the whole time to be honest. I was so hyped to hear You and I and Good Day, especially since I had heard that she was retiring her old music. Which was good because those songs were what I came to hear!!",
+      "This was my first time at a Kpop concert, so I was surprised at how structured and organized the whole concert set was. It seemed nice actually, especially since there was no waiting around for setup time or openers.. the show basically started right on time"
+    ],
+    poster_image: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754981542/GLMpEqLa0AAQFsB_p0ch82.jpg",
+    videos: [
+      { 
+        title: "Through the Night",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754982426/IMG_4619_lctilo.mov"
+      },
+      { 
+        title: "You and I",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754982459/IMG_4626_rg0wmh.mov"
+      },
+            { 
+        title: "Good Day",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754982443/IMG_4629_oainl3.mov"
+      }
+    ]
+  },
+  {
+    id: 10,
+    artist_name: "2NE1",
+    artist_photo: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754983190/cropped-cl-shares-new-2ne1-pictures-in-honor-of-their-15th-v0-kohdj0b12t0d1_nqfod8.jpg",
+    tour_name: "Head in the Clouds",
+    date: "June 1st, 2025",
+    location: "Pasadena, CA",
+    venue: "The Rose Bowl",
+    album_id: "5WyEkWi7ZPMrVSbU1Cabba",
+    album_ranking: [
+      { track_no: 1, rank: 2 },
+      { track_no: 2, rank: 1 },
+      { track_no: 3, rank: 5 },
+      { track_no: 4, rank: 4 },
+      { track_no: 5, rank: 6 },
+      { track_no: 6, rank: 7 },
+      { track_no: 7, rank: 3 },
+    ],
+    concert_notes: [
+      "This is definitely like a once in a lifetime event for me 🤯🫨 You thought IU was a defining moment in my childhood -- 2NE1 is on a whole other scale. I knew as soon as I saw this lineup drop that I would do anything to go see them, and so I did lol",
+      "Their presence was so powerful!! I think them being on tour in Asia prior to this really helped them build up their energy because they were not messing around. Although Bom wasn't there :')) I think the other three definitely gave more than enough to fill her absence",
+      "All of the bangers were played, every lyric was sung by me and everyone around me. The mini mosh pit that was formed during Ugly was something that I didn't know I needed. I definitely lost my voice the next day",
+      "I think I was in awe at how good Minzy sounded. I know all of them can sing (yes even Dara), but Minzy's voice just had so much power. They were also such fun performers, I loved how they kept walking down the catwalk to interact with people"
+    ],
+    poster_image: "https://res.cloudinary.com/dnlgbb2ny/image/upload/v1754983191/m4qxvdumuqbmiakzizbp_jx6obn.jpg",
+    videos: [
+      { 
+        title: "Intro + Fire",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983323/IMG_8636_ximr1g.mov"
+      },
+      { 
+        title: "Clap",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983286/IMG_8637_tdruxu.mov"
+      },
+      { 
+        title: "Can't Nobody",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983302/IMG_8638_mp24ra.mov"
+      },
+      { 
+        title: "I Don't Care",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983336/IMG_8648_ffos8p.mov"
+      },
+      { 
+        title: "I Don't Care (more)",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983267/IMG_8649_yzsqdb.mov"
+      },
+      { 
+        title: "CL",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983256/IMG_8650_o9c4ow.mov"
+      },
+      { 
+        title: "Lonely",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983323/IMG_8651_i41coe.mov"
+      },
+      { 
+        title: "I Am The Best",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983295/IMG_8658_ihacmf.mov"
+      },
+      { 
+        title: "Go Away",
+        src: "https://res.cloudinary.com/dnlgbb2ny/video/upload/v1754983289/IMG_8659_rkgi6s.mov"
+      },
+    ]
+  },
+  
 ];
 
 export const welcomeNotes: string[] = [
